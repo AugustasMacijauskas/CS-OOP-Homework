7014cc047f18fe18be65a84e6628d0cb034caee7
AugustasMacijauskas <august.macijauskas@gmail.com>
2018-05-23 20:11:07 +0300

    Klausimas 7

--- Kartojimas/Klausimas7/Program.cs
+++ Kartojimas/Klausimas7/Program.cs
@@ -42,31 +42,38 @@ namespace Klausimas5
 
                         if (paskutinisSuLyginiu != "" && paskutinisSuSkaitmeniu != "" && paskutinisSuLyginiu != paskutinisSuSkaitmeniu)
                         {
-                            Console.WriteLine(paskutinisSuLyginiu + " --> " + paskutinisSuSkaitmeniu);
-                            Match match1 = Regex.Match(line, $@"\b{paskutinisSuLyginiu}\b");
-                            Match match2 = Regex.Match(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+                            MatchCollection matches1 = Regex.Matches(line, $@"\b{paskutinisSuLyginiu}\b");
+                            MatchCollection matches2 = Regex.Matches(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+                            Match match1 = matches1[matches1.Count - 1];
+                            Match match2 = matches2[matches2.Count - 1];
                             if (match1.Index < match2.Index)
                             {
-                                Console.WriteLine(match1.Value + " ir " + match2.Value);
-                                Console.WriteLine("1");
+                                MatchCollection matches = Regex.Matches(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+                                Match match = matches[matches.Count - 1];
+                                line = line.Remove(match.Index, match.Length);
+                                line = line.Insert(match.Index, paskutinisSuLyginiu);
+
+                                matches = Regex.Matches(line, $@"\b{paskutinisSuLyginiu}\b");
+                                match = matches[matches.Count - 2];
+                                line = line.Remove(match.Index, match.Length);
+                                line = line.Insert(match.Index, paskutinisSuSkaitmeniu);
                             }
                             if (match1.Index > match2.Index)
                             {
-                                Console.WriteLine(match1.Value + " ir " + match2.Value);
-                                Console.WriteLine("2");
+                                MatchCollection matches = Regex.Matches(line, $@"\b{paskutinisSuLyginiu}\b");
+                                Match match = matches[matches.Count - 1];
+                                line = line.Remove(match.Index, match.Length);
+                                line = line.Insert(match.Index, paskutinisSuSkaitmeniu);
+
+                                matches = Regex.Matches(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+                                match = matches[matches.Count - 2];
+                                line = line.Remove(match.Index, match.Length);
+                                line = line.Insert(match.Index, paskutinisSuLyginiu);
                             }
-                            // line = new Regex($@"{}").Replace();
-                        }
-                        else if (paskutinisSuLyginiu != "" && paskutinisSuSkaitmeniu != "" && paskutinisSuLyginiu == paskutinisSuSkaitmeniu)
-                        {
-                            Console.WriteLine("Surastas tas pats žodis!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Kažkurio iš žodžių eilutėje nėra!");
                         }
 
                         ats.WriteLine(line);
+                        Console.WriteLine(line);
                     }
                 }
             }
@@ -124,3 +131,68 @@ namespace Klausimas5
        
     }
 }
+
+//static void RastiZTekste(string fr, string fv, string skyrikliai)
+//{
+//    using (var ats = File.CreateText(fv))
+//    {
+//        using (StreamReader reader = new StreamReader(fr))
+//        {
+//            string line = "pradzia";
+//            int i = 1;
+//            while ((line = reader.ReadLine()) != null)
+//            {
+//                // Panaudokite metodus, kurie randa paskutinį kiekvienos eilutės žodį, 
+//                // pasibaigiantį lyginiu skaitmeniu, ir metodą, kuris randa paskutinį
+//                // trumpiausią eilutės žodį, pasibaigiantį skaitmeniu.
+//                // Rastus žodžius sukeiskite vietomis. Skyrikliai nekeičiami.
+//                string paskutinisSuLyginiu = RastiPaskutinįSuLyginiu(line, skyrikliai);
+//                string paskutinisSuSkaitmeniu = RastiPaskutinįSuSkaitmeniu(line, skyrikliai);
+
+//                Console.WriteLine(i + " eilute   -----------------------------------------------------");
+//                Console.WriteLine(line);
+//                i++;
+
+//                if (paskutinisSuLyginiu != "" && paskutinisSuSkaitmeniu != "" && paskutinisSuLyginiu != paskutinisSuSkaitmeniu)
+//                {
+//                    Console.WriteLine(paskutinisSuLyginiu + " --> " + paskutinisSuSkaitmeniu);
+//                    Match match1 = Regex.Match(line, $@"\b{paskutinisSuLyginiu}\b");
+//                    Match match2 = Regex.Match(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+//                    if (match1.Index < match2.Index)
+//                    {
+//                        Console.WriteLine(match1.Index + " ir " + match2.Index);
+//                        Console.WriteLine(match1.Value + " ir " + match2.Value);
+//                        Console.WriteLine("1");
+//                        Match match = Regex.Match(line, $@"\b{paskutinisSuSkaitmeniu}\b");
+//                        line = line.Remove(match.Index, match.Length);
+//                        line = line.Insert(match.Index, paskutinisSuLyginiu);
+
+//                        line = new Regex($@"\b{paskutinisSuLyginiu}\b").Replace(line, paskutinisSuSkaitmeniu, 1);
+//                    }
+//                    if (match1.Index > match2.Index)
+//                    {
+//                        Console.WriteLine(match1.Index + " ir " + match2.Index);
+//                        Console.WriteLine(match1.Value + " ir " + match2.Value);
+//                        Console.WriteLine("2");
+//                        Match match = Regex.Match(line, $@"\b{paskutinisSuLyginiu}\b");
+//                        line = line.Remove(match.Index, match.Length);
+//                        line = line.Insert(match.Index, paskutinisSuSkaitmeniu);
+
+//                        line = new Regex($@"\b{paskutinisSuSkaitmeniu}\b").Replace(line, paskutinisSuLyginiu, 1);
+//                    }
+//                }
+//                else if (paskutinisSuLyginiu != "" && paskutinisSuSkaitmeniu != "" && paskutinisSuLyginiu == paskutinisSuSkaitmeniu)
+//                {
+//                    Console.WriteLine("Surastas tas pats žodis!");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Kažkurio iš žodžių eilutėje nėra!");
+//                }
+
+//                ats.WriteLine(line);
+//                Console.WriteLine(line);
+//            }
+//        }
+//    }
+//}
